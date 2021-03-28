b5a7ae5bc7078126589f79a36756a264caa458ec
noaione <noaione0809@gmail.com>
2021-03-28 18:14:15 +0700

    api(get): add stats and latestanime route

--- src/routes/api/get.ts
+++ src/routes/api/get.ts
@@ -30,6 +30,141 @@ APIGetRoutes.get("/", ensureLoggedIn("/"), async (req, res) => {
     }
 });
 
+function filterToNewestStatusOnly(fetchedData: ShowtimesProps) {
+    const animeSets = [];
+    fetchedData.anime.forEach((anime_data) => {
+        const newData = {};
+        newData["title"] = anime_data.title;
+        newData["start_time"] = anime_data.start_time;
+        newData["assignments"] = anime_data.assignments;
+        newData["poster"] = anime_data.poster_data.url;
+        let latestEpisode;
+        for (let ep = 0; ep < anime_data.status.length; ep++) {
+            const status_ep = anime_data.status[ep];
+            if (status_ep.is_done) {
+                continue;
+            }
+            if (isNone(latestEpisode)) {
+                latestEpisode = status_ep;
+                break;
+            }
+        }
+        if (isNone(latestEpisode)) {
+            return;
+        }
+        newData["status"] = latestEpisode;
+        animeSets.push(newData);
+    });
+    return animeSets;
+}
+
+function countAnimeStats(servers_data: ShowtimesProps[]) {
+    let animeCount = 0;
+    let rawProjectCount = 0;
+    const savedAnime = [];
+    servers_data.forEach((server) => {
+        server.anime.forEach((res) => {
+            if (!savedAnime.includes(res.id)) {
+                savedAnime.push(res.id);
+                animeCount++;
+            }
+            rawProjectCount++;
+        });
+    });
+    return [animeCount, rawProjectCount];
+}
+
+function countAdminStats(servers_data: ShowtimesProps[]) {
+    let adminCount = 0;
+    const savedAdmin = [];
+    servers_data.forEach((server) => {
+        server.serverowner.forEach((res) => {
+            if (!savedAdmin.includes(res)) {
+                savedAdmin.push(res);
+                adminCount++;
+            }
+        });
+    });
+    return adminCount;
+}
+
+APIGetRoutes.get("/stats", ensureLoggedIn("/"), async (req, res) => {
+    if (isNone(req.user)) {
+        res.status(403).json({ message: "Unauthorized", code: 403 });
+    } else {
+        const userData = req.user as UserProps;
+        if (userData.privilege === "owner") {
+            const fetchServers = await ShowtimesModel.find(
+                {},
+                {
+                    serverowner: 1,
+                    "anime.id": 1,
+                }
+            );
+            const totalServers = fetchServers.length;
+            const [totalAnime, rawProjectCount] = countAnimeStats(fetchServers);
+            const totalAdmin = countAdminStats(fetchServers);
+            res.json({
+                code: 200,
+                data: {
+                    servers: totalServers,
+                    anime: totalAnime,
+                    admins: totalAdmin,
+                    project: rawProjectCount,
+                },
+            });
+        } else {
+            const fetchServers = await ShowtimesModel.findOne(
+                { id: { $eq: userData.id } },
+                {
+                    "anime.status": 1,
+                }
+            );
+            const statsData = { finished: 0, unfinished: 0 };
+            fetchServers.anime.forEach((anime) => {
+                let anyUndone = false;
+                anime.status.forEach((res) => {
+                    if (res.is_done) {
+                        anyUndone = true;
+                    }
+                });
+                if (anyUndone) {
+                    statsData["unfinished"]++;
+                } else {
+                    statsData["finished"]++;
+                }
+            });
+            res.json({ data: statsData, code: 200 });
+        }
+    }
+});
+
+APIGetRoutes.get("/latestanime", ensureLoggedIn("/"), async (req, res) => {
+    if (isNone(req.user)) {
+        res.status(403).json({ message: "Unauthorized", code: 403 });
+    } else {
+        const userData = req.user as UserProps;
+        if (userData.privilege === "owner") {
+            res.status(501).json({
+                message: "Sorry, this API routes only available to non-admin account",
+                code: 501,
+            });
+        } else {
+            const fetchServers = await ShowtimesModel.findOne(
+                { id: { $eq: userData.id } },
+                {
+                    "anime.title": 1,
+                    "anime.assignments": 1,
+                    "anime.poster_data": 1,
+                    "anime.start_time": 1,
+                    "anime.status": 1,
+                }
+            );
+            res.json({ data: filterToNewestStatusOnly(fetchServers), code: 200 });
+        }
+    }
+});
+
 function filterToSpecificAnime(results: ShowtimesProps, anime_id: string) {
     const animeLists = results.anime.filter((res) => res.id === anime_id);
     return animeLists;
