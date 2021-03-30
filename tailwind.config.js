f98f62cac34c7e7e02a6f6e83aea1bef03fce17c
noaione <noaione0809@gmail.com>
2021-03-30 19:59:56 +0700

    Use "class" mode for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     purge: ["./public/**/*.{js,ts,jsx,tsx,ejs}", "./lib/**/*.{js,ts}", "./src/**/*.{js,ts,jsx,tsx,ejs}"],
-    darkMode: "media", // or 'media' or 'class'
+    darkMode: "class", // or 'media' or 'class'
     theme: {
         extend: {},
     },
