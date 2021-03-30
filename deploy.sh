a2db88b1aa73adc99beb7c8343119680abc5e394
noaione <noaione0809@gmail.com>
2021-03-30 14:06:56 +0100

    create deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Since I'm using NVM
+PATH=~/.nvm/versions/node/v14.15.1/bin:$PATH
+cd /var/www/naotimes-webui
+
+# Pull newest commit
+git pull
+
+# Install deps and build
+pnpm install
+pnpm run build
+
+# Restart pm2
+pm2 restart --silent naoTimesUI
+
