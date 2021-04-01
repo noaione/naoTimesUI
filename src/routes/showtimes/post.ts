345e3a9432035c6f7e9fed6e25a7ef4d89dd4d1f
noaione <noaione0809@gmail.com>
2021-04-01 14:48:48 +0700

    showtimes(post): handle project creation

--- src/routes/showtimes/post.ts
+++ src/routes/showtimes/post.ts
@@ -1,11 +1,69 @@
+import axios from "axios";
 import express from "express";
 import { ensureLoggedIn } from "connect-ensure-login";
+import { get, has } from "lodash";
 
 import { UserProps } from "../../models/user";
-import { isNone } from "../../lib/utils";
+import { ShowtimesModel } from "../../models/show";
+import { isNone, parseAnilistAPIResult, verifyExist } from "../../lib/utils";
 import { emitSocket, emitSocketAndWait } from "../../lib/socket";
+import { logger as MainLogger } from "../../lib/logger";
 
 const APIPOSTRoutes = express.Router();
+
+const AnimeInfoQuery = `
+query ($id:Int!) {
+    Media(id:$id,type:ANIME) {
+        id
+        idMal
+        title {
+            romaji
+            english
+            native
+        }
+        coverImage {
+            extraLarge
+            large
+            medium
+            color
+        }
+        format
+        episodes
+        startDate {
+            year
+            month
+            day
+        }
+        airingSchedule {
+            nodes {
+                id
+                episode
+                airingAt
+            }
+        }
+    }
+}
+`;
+
+async function getAnimeInfo(anime_id: string | number): Promise<any> {
+    if (typeof anime_id !== "number") {
+        anime_id = parseInt(anime_id);
+        if (isNaN(anime_id)) return {};
+    }
+    const req = await axios.post(
+        "https://graphql.anilist.co",
+        { query: AnimeInfoQuery, variables: { id: anime_id } },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            responseType: "json",
+        }
+    );
+    const res = req.data;
+    return get(res, "data.Media", {});
+}
 
 APIPOSTRoutes.post("/serverinfo", ensureLoggedIn("/"), async (req, res) => {
     if (isNone(req.user)) {
@@ -35,7 +93,136 @@ APIPOSTRoutes.post("/poll", ensureLoggedIn("/"), (req, res) => {
     }
 });
 
-APIPOSTRoutes.post("/anime", ensureLoggedIn("/"), async (req, res) => {
+async function addNewProject(dataToAdd: any) {
+    if (!verifyExist(dataToAdd, "server", "string")) {
+        return { success: false, message: "missing server key on request, or server key is not a string" };
+    }
+    if (!verifyExist(dataToAdd, "roles", "array")) {
+        return { success: false, message: "missing roles key that should contain array" };
+    }
+    if (!verifyExist(dataToAdd, "anime", "object")) {
+        return { success: false, message: "missing anime key that should be an object" };
+    }
+    const VALID_ROLES = ["TL", "TLC", "ENC", "ED", "TM", "TS", "QC"];
+    const animeData = dataToAdd["anime"];
+    const serverId: string = dataToAdd["server"];
+    const logger = MainLogger.child({ cls: "ShowtimesPOST", fn: `AddNewProject[${serverId}]` });
+    const animeId = animeData["id"];
+    const animeName = animeData["name"];
+    const expectedEpisode: number = animeData["episode"];
+    if (isNone(animeId) || isNone(animeName)) {
+        return { success: false, message: "missing `id` or `name` or `episode` key on `anime` object" };
+    }
+    logger.info("Checking if anime exist on the database");
+    const showServer = await ShowtimesModel.findOne({ id: { $eq: serverId } }, { "anime.id": 1 });
+    let isExist = false;
+    showServer.anime.forEach((res) => {
+        if (res.id === animeId) {
+            isExist = true;
+        }
+    });
+    if (isExist) {
+        logger.warn("Anime already exist, returning!");
+        return {
+            success: false,
+            message: "Anime sudah terdaftar di daftar garapan, mohon gunakan mode edit!",
+        };
+    }
+    const addedRoles = dataToAdd["roles"];
+    const validatedRoles = {
+        TL: {
+            id: null,
+            name: null,
+        },
+        TLC: {
+            id: null,
+            name: null,
+        },
+        ENC: {
+            id: null,
+            name: null,
+        },
+        ED: {
+            id: null,
+            name: null,
+        },
+        TM: {
+            id: null,
+            name: null,
+        },
+        TS: {
+            id: null,
+            name: null,
+        },
+        QC: {
+            id: null,
+            name: null,
+        },
+    };
+    const cachedUserID = {};
+    for (let i = 0; i < addedRoles.length; i++) {
+        const element = addedRoles[i];
+        if (VALID_ROLES.includes(element.role.toUpperCase()) && !isNone(element.id)) {
+            if (!has(cachedUserID, element.id)) {
+                try {
+                    logger.info(`Requesting user info for role ${element.role} with ID ${element.id}`);
+                    const userData = await emitSocketAndWait("get user", element.id);
+                    validatedRoles[element.role]["id"] = element.id;
+                    if (userData.name) {
+                        validatedRoles[element.role]["name"] = userData.name;
+                        cachedUserID[element.id] = userData.name;
+                    } else {
+                        cachedUserID[element.id] = null;
+                    }
+                } catch (e) {
+                    logger.error(`Failed to fetch user ${element.id}, reason: ${e.toString()}`);
+                    validatedRoles[element.role]["id"] = element.id;
+                    cachedUserID[element.id] = null;
+                }
+            } else {
+                logger.info(`Using cached data for ${element.role} with ID ${element.id}`);
+                validatedRoles[element.role]["id"] = element.id;
+                validatedRoles[element.role]["name"] = get(cachedUserID, element.id, null);
+            }
+        }
+    }
+    let generatedRole;
+    try {
+        logger.info(`Generating new role on server with name: ${animeName}`);
+        generatedRole = await emitSocketAndWait("create role", { id: serverId, name: animeName });
+    } catch (e) {
+        logger.error(`Failed to generate new role, reason: ${e.toString()}`);
+        return { success: false, message: e.toString() };
+    }
+
+    const roleId = generatedRole["id"];
+    logger.info(`Fetching Anime information for ${animeId}`);
+    const apiRequest = await getAnimeInfo(animeId);
+    if (Object.keys(apiRequest).length < 1) {
+        logger.error("Failed to fetch the API, got no results!");
+        return { success: false, message: "Gagal mendapatkan dari Anilist, mohon coba lagi nanti!" };
+    }
+    logger.info("Parsing results...");
+    const finalizedAnimeData = parseAnilistAPIResult(apiRequest, expectedEpisode);
+    finalizedAnimeData["role_id"] = roleId;
+    finalizedAnimeData["assignments"] = validatedRoles;
+    logger.info("Updating by pushing the new finalized data to the database...");
+    try {
+        await ShowtimesModel.update({ id: { $eq: serverId } }, { $addToSet: { anime: finalizedAnimeData } });
+    } catch (err) {
+        logger.error("Failed to update the server state, returning an undefined data...");
+        console.error(err);
+        return { success: false, message: "Gagal memperbaharui database, mohon coba lagi nanti" };
+    }
+    logger.info("Emitting pull data request for server to Bot");
+    emitSocket("pull data", serverId);
+    return { success: true, message: "success" };
+}
+
+APIPOSTRoutes.use(express.json());
+
+APIPOSTRoutes.post("/projek", ensureLoggedIn("/"), async (req, res) => {
+    const reqData = req.body;
     if (isNone(req.user)) {
         res.status(403).json({ message: "Unauthorized", code: 403 });
     } else {
@@ -43,7 +230,26 @@ APIPOSTRoutes.post("/anime", ensureLoggedIn("/"), async (req, res) => {
         if (userData.privilege === "owner") {
             res.status(504).json({ done: false });
         } else {
-            res.status(504).json({ done: false });
+            const result = await addNewProject(reqData);
+            res.json({ ...result });
+        }
+    }
+});
+
+APIPOSTRoutes.post("/authsocket", ensureLoggedIn("/"), async (req, res) => {
+    if (isNone(req.user)) {
+        res.status(403).json({ message: "Unauthorized", code: 403 });
+    } else {
+        const userData = req.user as UserProps;
+        if (userData.privilege === "owner") {
+            try {
+                await emitSocketAndWait("authenticate", process.env.BOT_SOCKET_PASSWORD);
+                res.json({ done: true, message: "done" });
+            } catch (e) {
+                res.status(500).json({ done: false, message: e.toString() });
+            }
+        } else {
+            res.status(403).json({ done: false, message: "admin only" });
         }
     }
 });
