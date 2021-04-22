a2cb2cc176f0f5322926a29c625bab5ebd75c7aa
noaione <noaione0809@gmail.com>
2021-04-22 10:11:35 +0700

    Create some layout component for Admin page

---
+++ components/AdminLayout.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { UserProps } from "../models/user";
+import AdminHeader from "./AdminHeader";
+import AdminSidenav from "./AdminSidenav";
+
+import packageJSON from "../package.json";
+
+interface AdminLayoutProps {
+    user: UserProps;
+    active?: "home" | "project" | "projectpage" | "settings";
+    title?: string;
+    overflowX?: boolean;
+}
+
+interface AdminLayoutState {
+    sidebarOpen: boolean;
+}
+
+class AdminLayout extends React.Component<AdminLayoutProps, AdminLayoutState> {
+    constructor(props: AdminLayoutProps) {
+        super(props);
+        this.openSidebar = this.openSidebar.bind(this);
+        this.closeSidebar = this.closeSidebar.bind(this);
+        this.state = {
+            sidebarOpen: false,
+        };
+    }
+
+    closeSidebar() {
+        this.setState({ sidebarOpen: false });
+    }
+
+    openSidebar() {
+        this.setState({ sidebarOpen: true });
+    }
+
+    render() {
+        const { version } = packageJSON;
+        const { user, active, title, overflowX } = this.props;
+        const realTitle = title || "Ikhtisar";
+        let realOverflowX = false;
+        if (typeof overflowX === "boolean") {
+            realOverflowX = overflowX;
+        }
+        const appInfo = { semver: version };
+        return (
+            <>
+                <div className="flex h-screen bg-gray-100 dark:bg-gray-800 font-roboto transition-colors duration-300">
+                    <AdminSidenav
+                        id={user.id}
+                        name={user.name}
+                        privilige={user.privilege}
+                        show={this.state.sidebarOpen}
+                        active={active}
+                        onClose={this.closeSidebar}
+                        appInfo={appInfo}
+                    />
+                    <div className="flex-1 flex flex-col overflow-hidden">
+                        <AdminHeader
+                            id={user.id}
+                            name={user.name}
+                            privilige={user.privilege}
+                            title={realTitle}
+                            onOpen={this.openSidebar}
+                        />
+                        <main
+                            id="root"
+                            className={
+                                realOverflowX
+                                    ? "flex-1 overflow-x-auto overflow-y-auto"
+                                    : "flex-1 overflow-x-hidden overflow-y-auto"
+                            }
+                        >
+                            {this.props.children}
+                        </main>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
+
+export default AdminLayout;
---
+++ components/AdminSidenav.tsx
@@ -0,0 +1,146 @@
+import React from "react";
+
+import CogIcon from "mdi-react/CogIcon";
+import HomeIcon from "mdi-react/HomeIcon";
+import PencilIcon from "mdi-react/PencilIcon";
+import ServerIcon from "mdi-react/ServerIcon";
+
+import GitHubIcon from "./Icons/GitHub";
+
+import { romanizeNumber } from "../lib/utils";
+
+interface SidenavProps {
+    id: string;
+    name?: string;
+    privilige: "owner" | "server";
+    show: boolean;
+    active?: "home" | "project" | "projectpage" | "settings";
+    appInfo?: {
+        semver?: string;
+        commit?: string;
+    };
+    onClose: () => void;
+}
+
+class AdminSidenav extends React.Component<SidenavProps, {}> {
+    constructor(props: SidenavProps) {
+        super(props);
+        this.callOnClose = this.callOnClose.bind(this);
+        this.closeSidebar = this.closeSidebar.bind(this);
+    }
+
+    callOnClose() {
+        this.props.onClose();
+    }
+
+    closeSidebar() {
+        this.callOnClose();
+    }
+
+    render() {
+        const { privilige, active, show } = this.props;
+        const appInfo = this.props.appInfo || {};
+        const { semver, commit } = appInfo;
+        const isAdmin = privilige === "owner";
+        const curActive = active || "home";
+
+        const blackLucent = show ? "block" : "hidden";
+        const sidebarTransition = show ? "translate-x-0 ease-out" : "-translate-x-full ease-in";
+
+        const adminPageUrl = isAdmin ? "/admin/peladen" : "/admin/proyek";
+        const adminPageIcon = isAdmin ? (
+            <ServerIcon className="text-sm" />
+        ) : (
+            <PencilIcon className="text-sm" />
+        );
+
+        const currentCopyright = new Date().getUTCFullYear();
+        const romanizedCC = romanizeNumber(currentCopyright);
+
+        const NormanClass =
+            "py-2 pl-2 text-sm text-gray-600 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-800 rounded flex flex-row items-center";
+        const SelClass =
+            "py-2 pl-2 text-sm text-gray-700 dark:text-gray-100 bg-gray-200 dark:bg-gray-800 rounded flex flex-row items-center";
+
+        return (
+            <>
+                <div
+                    className={
+                        "fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden " + blackLucent
+                    }
+                    onClick={this.closeSidebar}
+                ></div>
+                <div
+                    className={
+                        "fixed z-30 inset-y-0 left-0 w-60 transition duration-300 transform bg-white dark:bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0 " +
+                        sidebarTransition
+                    }
+                >
+                    <div className="flex items-center justify-center mt-8">
+                        <div className="flex items-center">
+                            <span className="text-gray-800 dark:text-white text-2xl font-semibold">
+                                naoTimes
+                            </span>
+                        </div>
+                    </div>
+
+                    <nav className="flex flex-col mt-10 px-4">
+                        <a
+                            href={curActive === "home" ? "#" : "/admin"}
+                            className={curActive === "home" ? SelClass : NormanClass}
+                        >
+                            <HomeIcon className="text-sm" />
+                            <span className="ml-1">Ikhtisar</span>
+                        </a>
+                        <a
+                            href={curActive === "project" ? "#" : adminPageUrl}
+                            className={curActive === "project" ? SelClass + " mt-3" : NormanClass + " mt-3"}
+                        >
+                            {adminPageIcon}
+                            <span className="ml-1">{isAdmin ? "Peladen" : "Proyek"}</span>
+                        </a>
+                        <a
+                            href={curActive === "settings" ? "#" : "/admin/atur"}
+                            className={curActive === "settings" ? SelClass + " mt-3" : NormanClass + " mt-3"}
+                        >
+                            <CogIcon className="text-sm" />
+                            <span className="ml-1">Pengaturan</span>
+                        </a>
+                    </nav>
+
+                    <div className="absolute items-end justify-start bottom-0 left-0 ml-4 mb-4 z-10">
+                        {semver && (
+                            <>
+                                <div className="dark:text-gray-200 font-semibold flex flex-row mb-2 text-lg items-center">
+                                    <a
+                                        className="text-gray-900 dark:text-gray-100 hover:opacity-70 transition-opacity duration-200"
+                                        href="https://github.com/naoTimesdev/webpanel"
+                                    >
+                                        <GitHubIcon />
+                                    </a>
+                                    <span className="ml-2">v{semver}</span>
+                                </div>
+                            </>
+                        )}
+                        {commit && (
+                            <>
+                                <div className="dark:text-gray-200 font-semibold">
+                                    Commit:{" "}
+                                    <a
+                                        className="text-gray-900 dark:text-gray-100 opacity-80 transition-opacity duration-300"
+                                        href={"https://github.com/naoTimesdev/webpanel/commit/" + commit}
+                                    >
+                                        {commit.slice(0, 7)}
+                                    </a>
+                                </div>
+                            </>
+                        )}
+                        <div className="dark:text-gray-200">© {romanizedCC} - naoTimesDev</div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
+
+export default AdminSidenav;
