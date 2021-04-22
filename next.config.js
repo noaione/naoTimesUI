b6003e5057cf034001834241561ba8ce32444bdf
noaione <noaione0809@gmail.com>
2021-04-22 10:24:06 +0700

    Remove next-build-id

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextBuildId = require("next-build-id");
 
 module.exports = {
     future: {
         webpack5: true,
     },
-    generateBuildId: nextBuildId({ dir: __dirname }),
 };
