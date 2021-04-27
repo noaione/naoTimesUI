7cca8e90af4dd022c1937ec2b59e58b600f0fd7c
noaione <noaione0809@gmail.com>
2021-04-27 17:49:01 +0700

    typing: extend the process.env to includes .env stuff

--- next-env.d.ts
+++ next-env.d.ts
@@ -13,5 +13,14 @@ declare global {
         interface Global {
             mongoose?: MongooseCached;
         }
+
+        // Extend process.env typing
+        interface ProcessEnv {
+            MONGODB_URI?: string;
+            TOKEN_SECRET?: string;
+            BOT_SOCKET_HOST?: string;
+            BOT_SOCKET_PORT?: string;
+            BOT_SOCKET_PASSWORD?: string;
+        }
     }
 }
