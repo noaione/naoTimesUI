2206087d687e3664c0e4ab12abab153e81bf2f9f
noaione <noaione0809@gmail.com>
2021-04-27 17:27:27 +0700

    typing: properly declare the global mongoose cache

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -6,20 +6,15 @@ if (!MONGODB_URI) {
     throw new Error(`Please define MONGODB_URI on your environment table`);
 }
 
-// @ts-ignore
 if (!global.mongoose) {
-    // @ts-ignore
     global.mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
-    // @ts-ignore
     if (global.mongoose.conn) {
-        // @ts-ignore
         return global.mongoose.conn;
     }
 
-    // @ts-ignore
     if (!global.mongoose.promise) {
         const opts = {
             useNewUrlParser: true,
@@ -30,14 +25,11 @@ async function dbConnect() {
             useCreateIndex: true,
         };
 
-        // @ts-ignore
         global.mongoose.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
             return mongoose;
         });
     }
-    // @ts-ignore
     global.mongoose.conn = await global.mongoose.promise;
-    // @ts-ignore
     return global.mongoose.conn;
 }
 
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,17 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+import { Mongoose } from "mongoose";
+
+interface MongooseCached {
+    promise?: Promise<Mongoose> | null;
+    conn?: Mongoose | null;
+}
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            mongoose?: MongooseCached;
+        }
+    }
+}
