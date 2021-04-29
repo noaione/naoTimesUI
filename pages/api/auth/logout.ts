1e60c2c2dd0aa4e70be30497e9a18782ce9de6d6
noaione <noaione0809@gmail.com>
2021-04-29 16:03:54 +0700

    api(logout): change from redirect to normal json output

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -5,5 +5,5 @@ import withSession, { NextApiRequestWithSession } from "../../../lib/session";
 export default withSession(async (req: NextApiRequestWithSession, res: NextApiResponse) => {
     await req.session.destroy();
     res.setHeader("cache-control", "no-store, max-age=0");
-    res.redirect("/");
+    res.json({ loggedIn: false });
 });
