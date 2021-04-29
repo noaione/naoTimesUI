9bef82163564b7cf676ab8ff408bdc358bad5c08
noaione <noaione0809@gmail.com>
2021-04-29 20:25:57 +0700

    allow multiline comment on HTMLComment can be manually set, or automatically detected

--- components/HTMLComment.tsx
+++ components/HTMLComment.tsx
@@ -2,6 +2,27 @@ import React from "react";
 
 interface CommentProps {
     text?: string;
+    multiLine?: boolean;
+}
+
+function selectText<T>(text?: T): string {
+    if (typeof text !== "string") {
+        return "Commented from <React />";
+    }
+    if (text.trim().length < 1) {
+        return "Commented from <React />";
+    }
+    return text.trim();
+}
+
+function isMultiline(text?: string, forceMulti?: boolean): boolean {
+    if (forceMulti) {
+        return true;
+    }
+    if (typeof text === "string" && text.includes("\n")) {
+        return true;
+    }
+    return false;
 }
 
 class HTMLComment extends React.Component<CommentProps> {
@@ -11,9 +32,24 @@ class HTMLComment extends React.Component<CommentProps> {
         super(props);
         this.ref = React.createRef();
     }
+
     componentDidMount() {
+        const { text, multiLine } = this.props;
         // Add comment before the <div> element
-        this.ref.current.outerHTML = `<!-- ${this.props.text || "React Comment"} -->`;
+        let comment = "<!--";
+        if (isMultiline(text, multiLine)) {
+            comment += "\n\t";
+        } else {
+            comment += " ";
+        }
+        comment += selectText(text);
+        if (isMultiline(text, multiLine)) {
+            comment += "\n-->";
+        } else {
+            comment += " -->";
+        }
+
+        this.ref.current.outerHTML = comment;
         // Delete the <div> element afterwards
         this.ref.current.remove();
     }
