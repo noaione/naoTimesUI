7320a523dca19f3865eba9315f70d864782b86d5
noaione <noaione0809@gmail.com>
2021-04-29 16:06:34 +0700

    Properly use API routes to reset/change password Now with password strength check
    Requirement:
    - 8 character minimum
    - Minimum of uppercase letter

--- components/SettingsPage/ResetPassword.tsx
+++ components/SettingsPage/ResetPassword.tsx
@@ -8,6 +8,19 @@ interface RPassState {
     oldValue: string;
     newValue: string;
     isSubmitting: boolean;
+    isValid: boolean;
+    validityCheck: string[];
+}
+
+function verifyPasswordStrength(password: string): string[] {
+    const failureCheck = [];
+    if (password.length < 8) {
+        failureCheck.push("Password minimal 8 karakter");
+    }
+    if (!password.match(/[A-Z]/)) {
+        failureCheck.push("Minimal ada satu huruf kapital");
+    }
+    return failureCheck;
 }
 
 class ResetPasswordComponent extends React.Component<SettingsProps, RPassState> {
@@ -15,15 +28,21 @@ class ResetPasswordComponent extends React.Component<SettingsProps, RPassState> 
         super(props);
 
         this.submitPassword = this.submitPassword.bind(this);
+        this.passwordVerify = this.passwordVerify.bind(this);
 
         this.state = {
             oldValue: "",
             newValue: "",
             isSubmitting: false,
+            isValid: false,
+            validityCheck: verifyPasswordStrength(""),
         };
     }
 
     async submitPassword() {
+        if (this.state.isSubmitting || !this.state.isValid) {
+            return;
+        }
         this.setState({ isSubmitting: true });
         // eslint-disable-next-line @typescript-eslint/no-this-alias
         const outerThis = this;
@@ -32,8 +51,27 @@ class ResetPasswordComponent extends React.Component<SettingsProps, RPassState> 
         }, 2000);
     }
 
+    passwordVerify(passwd: string) {
+        this.setState({ newValue: passwd });
+        const totalError = verifyPasswordStrength(passwd);
+        if (totalError.length > 0) {
+            this.setState({ isValid: false, validityCheck: totalError });
+        } else {
+            this.setState({ isValid: true, validityCheck: [] });
+        }
+    }
+
     render() {
-        const { isSubmitting } = this.state;
+        const { isSubmitting, isValid, validityCheck } = this.state;
+        let disableButton = false;
+        if (!isValid) {
+            disableButton = true;
+        }
+        if (isSubmitting) {
+            disableButton = true;
+        }
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
+        const outerThis = this;
         return (
             <>
                 <div className="flex flex-col py-1">
@@ -49,7 +87,9 @@ class ResetPasswordComponent extends React.Component<SettingsProps, RPassState> 
                                     type="password"
                                     className="form-input w-full py-1 rounded-lg border-2 transition-colors duration-200 ease-in-out border-gray-200 focus:border-yellow-600 focus:outline-none"
                                     placeholder="************"
-                                    onChange={(ev) => this.setState({ oldValue: toString(ev.target.value) })}
+                                    onChange={(ev) =>
+                                        outerThis.setState({ oldValue: toString(ev.target.value) })
+                                    }
                                 />
                             </div>
                             <div className="w-full mt-2 mb-1 flex flex-col">
@@ -61,14 +101,22 @@ class ResetPasswordComponent extends React.Component<SettingsProps, RPassState> 
                                     value={this.state.newValue}
                                     className="form-input w-full py-1 rounded-lg border-2 transition-colors duration-200 ease-in-out border-gray-200 focus:border-yellow-600 focus:outline-none"
                                     placeholder="************"
-                                    onChange={(ev) => this.setState({ newValue: toString(ev.target.value) })}
+                                    onChange={(ev) => outerThis.passwordVerify(toString(ev.target.value))}
                                 />
+                                {!isValid &&
+                                    validityCheck.map((res) => {
+                                        return (
+                                            <p key={`errpass-${res}`} className="text-sm text-red-500 mt-1">
+                                                {res}
+                                            </p>
+                                        );
+                                    })}
                             </div>
                             <div className="flex mt-2">
                                 <button
                                     onClick={this.submitPassword}
                                     className={`rounded text-white px-4 py-2 ${
-                                        isSubmitting
+                                        disableButton
                                             ? "bg-blue-500 cursor-not-allowed"
                                             : "bg-blue-600 hover:bg-blue-700"
                                     } transition-colors duration-200 flex flex-row items-center focus:outline-none`}
