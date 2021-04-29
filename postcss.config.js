f78c585bd9fe499b1f9450617efbf0173024d326
noaione <noaione0809@gmail.com>
2021-04-29 23:50:10 +0700

    package: add nested postcss and react markdown

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
     plugins: {
         "postcss-import": {},
+        "postcss-nested": {},
         tailwindcss: {},
         autoprefixer: {},
         cssnano: isProd ? {} : false,
