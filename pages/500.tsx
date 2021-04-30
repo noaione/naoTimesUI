bf3ad54f869054947ecb22cad8dd09aa3db429ac
noaione <noaione0809@gmail.com>
2021-04-30 11:31:14 +0700

    chore: sort import

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
-
 import Image from "next/image";
+
 import MetadataHead from "../components/MetadataHead";
 
 class ServerErrorPage extends React.Component {
