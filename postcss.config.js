501cee67d9f3e3d8a7308ced65b5e8c5cccb0889
noaione <noaione0809@gmail.com>
2021-04-30 11:23:24 +0700

    try to fix postcss error

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,5 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-    plugins: {
-        "postcss-import": {},
-        "postcss-nested": {},
-        tailwindcss: {},
-        autoprefixer: {},
-        cssnano: isProd ? {} : false,
-    },
+    plugins: ["postcss-import", "postcss-nested", "tailwindcss", "autoprefixer", isProd ? "cssnano" : null],
 };
