dd8a675efa5a640af261070f88f271db834771c9
noaione <noaione0809@gmail.com>
2021-05-02 13:35:30 +0700

    create a discord preview generator

---
+++ components/FansubRSS/PreviewGenerator/embed.tsx
@@ -0,0 +1,345 @@
+/**
+ * This is a typescript version specifically made for naoTimesUI
+ * Adapter from https://github.com/leovoel/embed-visualizer/blob/master/src/components/embed.jsx
+ */
+import React from "react";
+import { DateTime } from "luxon";
+import { parseAllowLinks, parseEmbedTitle } from "./markdown";
+import { extractRGB } from "./color";
+
+interface LinkElem extends Partial<Omit<HTMLLinkElement, "children">> {
+    children: React.ReactNode;
+}
+
+class Link extends React.PureComponent<LinkElem> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { children, ...props } = this.props;
+        return (
+            // @ts-ignore
+            <a target="_blank" rel="noreferrer noopener" {...props}>
+                {children}
+            </a>
+        );
+    }
+}
+
+interface IEmbedColorPill {
+    color?: number;
+}
+
+class EmbedColorPill extends React.Component<IEmbedColorPill> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { color } = this.props;
+        let computed;
+        if (color) {
+            const c = extractRGB(color);
+            computed = `rgba(${c.r},${c.g},${c.b},1)`;
+        }
+
+        const style = { backgroundColor: computed !== undefined ? computed : "" };
+        return <div className="embed-color-pill" style={style} />;
+    }
+}
+
+interface IEmbedTitle {
+    title?: string;
+    url?: string;
+}
+
+class EmbedTitle extends React.Component<IEmbedTitle> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { title, url } = this.props;
+        if (!title) {
+            return null;
+        }
+
+        const titleParsed = parseEmbedTitle(title);
+
+        if (url) {
+            return (
+                <Link href={url} className="embed-color-pill">
+                    {titleParsed}
+                </Link>
+            );
+        }
+
+        return <div className="embed-title">{titleParsed}</div>;
+    }
+}
+
+interface IEmbedDescription {
+    content?: string;
+}
+
+class EmbedDescription extends React.Component<IEmbedDescription> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { content } = this.props;
+        if (!content) {
+            return null;
+        }
+
+        return <div className="embed-description markup">{parseAllowLinks(content)}</div>;
+    }
+}
+
+interface IEmbedAuthor {
+    name?: string;
+    url?: string;
+    icon_url?: string;
+}
+
+class EmbedAuthor extends React.Component<IEmbedAuthor> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { name, url, icon_url } = this.props;
+        if (!name) {
+            return null;
+        }
+
+        let authorName;
+        if (name) {
+            authorName = <span className="embed-author-name">{name}</span>;
+            if (url) {
+                authorName = (
+                    <Link href={url} className="embed-author-name">
+                        {name}
+                    </Link>
+                );
+            }
+        }
+
+        const authorIcon = icon_url ? (
+            <img src={icon_url} role="presentation" className="embed-author-icon" />
+        ) : null;
+        return (
+            <div className="embed-author">
+                {authorIcon}
+                {authorName}
+            </div>
+        );
+    }
+}
+
+interface IEmbedField {
+    name?: string;
+    value?: string;
+    inline: boolean;
+}
+
+class EmbedField extends React.Component<IEmbedField> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { name, value, inline } = this.props;
+        if (!name && !value) {
+            return null;
+        }
+
+        const classExtra = "embed-field" + (inline ? " embed-field-inline" : "");
+
+        const fieldName = name ? <div className="embed-field-name">{parseEmbedTitle(name)}</div> : null;
+        const fieldValue = value ? (
+            <div className="embed-field-value markup">{parseAllowLinks(name)}</div>
+        ) : null;
+
+        return (
+            <div className={classExtra}>
+                {fieldName}
+                {fieldValue}
+            </div>
+        );
+    }
+}
+
+interface IEmbedThumb {
+    url?: string;
+}
+
+class EmbedThumbnail extends React.Component<IEmbedThumb> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { url } = this.props;
+        if (!url) {
+            return null;
+        }
+
+        return (
+            <img
+                src={url}
+                className="embed-rich-thumb"
+                style={{ maxWidth: 80, maxHeight: 80 }}
+                role="presentation"
+            />
+        );
+    }
+}
+
+interface IEmbedImage {
+    url?: string;
+}
+
+class EmbedImage extends React.Component<IEmbedImage> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { url } = this.props;
+        if (!url) {
+            return null;
+        }
+
+        return (
+            <a className="embed-thumbnail embed-thumbnail-rich">
+                <img className="image" role="presentation" src={url} />
+            </a>
+        );
+    }
+}
+
+interface IEmbedFooter {
+    text?: string;
+    timestamp: number;
+    icon_url?: string;
+}
+
+class EmbedFooter extends React.Component<IEmbedFooter> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { text, timestamp, icon_url } = this.props;
+        if (!text && !timestamp) {
+            return null;
+        }
+
+        let timeText;
+        if (timestamp) {
+            const time = DateTime.fromMillis(timestamp);
+            // ddd MMM Do, YYYY [at] h:mm A
+            timeText = time.isValid ? time.toFormat("EEE MMM dd, yyyy 'at' hh:mm a") : undefined;
+        }
+
+        const footerText = [text, timeText].filter((e) => typeof e === "string").join(" | ");
+        const footerIcon =
+            text && icon_url ? (
+                <img
+                    src={icon_url}
+                    className="embed-footer-icon"
+                    role="presentation"
+                    width={20}
+                    height={20}
+                />
+            ) : null;
+
+        return (
+            <div>
+                {footerIcon}
+                <span className="embed-footer">{footerText}</span>
+            </div>
+        );
+    }
+}
+
+interface IEmbedFields {
+    fields: IEmbedField[];
+}
+
+class EmbedFields extends React.Component<IEmbedFields> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { fields } = this.props;
+        return (
+            <div className="embed-fields">
+                {fields.map((f, i) => (
+                    <EmbedField key={`field-${i}`} {...f} />
+                ))}
+            </div>
+        );
+    }
+}
+
+export interface IEmbed {
+    color?: number;
+    author?: IEmbedAuthor;
+    title?: string;
+    url?: string;
+    description?: string;
+    thumbnail?: IEmbedThumb;
+    image?: IEmbedImage;
+    timestamp?: number;
+    footer?: IEmbedFooter;
+    fields?: IEmbedField[];
+}
+
+export default class EmbedPreview extends React.Component<IEmbed> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {
+            color,
+            author,
+            title,
+            url,
+            description,
+            fields,
+            thumbnail,
+            image,
+            timestamp,
+            footer,
+        } = this.props;
+
+        return (
+            <>
+                <div className="accessory">
+                    <div className="embed-wrapper">
+                        <EmbedColorPill color={color} />
+                        <div className="embed embed-rich">
+                            <div className="embed-content">
+                                <div className="embed-content-inner">
+                                    <EmbedAuthor {...author} />
+                                    <EmbedTitle title={title} url={url} />
+                                    <EmbedDescription content={description} />
+                                    <EmbedFields fields={fields} />
+                                </div>
+                                <EmbedThumbnail {...thumbnail} />
+                            </div>
+                            <EmbedImage {...image} />
+                            <EmbedFooter timestamp={timestamp} {...footer} />
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
---
+++ components/FansubRSS/PreviewGenerator/markdown.tsx
@@ -0,0 +1,451 @@
+/**
+ * This is a typescript version specifically made for naoTimesUI
+ * Also removed some deprecated function.
+ * Adapted from https://github.com/leovoel/embed-visualizer/blob/master/src/components/markdown.jsx
+ */
+import React from "react";
+import SimpleMarkdown from "simple-markdown";
+import hljs from "highlight.js";
+import Twemoji from "twemoji";
+
+import Emoji from "../../../lib/constant/emoji";
+
+function flattenAst(node: SimpleMarkdown.ASTNode, parent?: SimpleMarkdown.SingleASTNode) {
+    if (Array.isArray(node)) {
+        for (let n = 0; n < node.length; n++) {
+            node[n] = flattenAst(node[n], parent);
+        }
+        return node;
+    }
+
+    if (node.content !== null) {
+        node.content = flattenAst(node.content, node);
+    }
+    if (parent !== null && node.type === parent.type) {
+        return node.content;
+    }
+    return node;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function astToString(node: SimpleMarkdown.ASTNode, result?: any[]) {
+    function inner(node: SimpleMarkdown.ASTNode, result = []) {
+        if (Array.isArray(node)) {
+            node.forEach((subNode) => astToString(subNode, result));
+        } else if (typeof node.content === "string") {
+            result.push(node.content);
+        } else if (node.content !== null) {
+            astToString(node.content, result);
+        }
+        return result;
+    }
+    return inner(node).join("");
+}
+
+type TransformFunction = (ast: SimpleMarkdown.SingleASTNode[]) => SimpleMarkdown.SingleASTNode[];
+
+function parserFor(rules: SimpleMarkdown.ParserRules, returnAst?: boolean) {
+    const parser = SimpleMarkdown.parserFor(rules);
+    // @ts-ignore
+    const renderer = SimpleMarkdown.outputFor(rules, "react");
+
+    return function (
+        input: string = "",
+        inline = true,
+        state: SimpleMarkdown.State = {},
+        transform: TransformFunction = null
+    ) {
+        if (!inline) {
+            input += "\n\n";
+        }
+
+        let ast = parser(input, { inline, ...state });
+        ast = flattenAst(ast);
+        if (transform) {
+            ast = transform(ast);
+        }
+        if (returnAst) {
+            return ast;
+        }
+        return renderer(ast);
+    };
+}
+
+function omit(object: any, excluded?: string[]) {
+    return Object.keys(object).reduce((result, key) => {
+        if (excluded.indexOf(key) === -1) {
+            result[key] = object[key];
+        }
+        return result;
+    }, {});
+}
+
+interface EmoteType {
+    id: string;
+}
+
+function getEmoteURL(emote: EmoteType) {
+    return `https://cdn.discordapp.com/emojis/${emote.id}.png`;
+}
+
+function getEmojiURL(surrogate?: string) {
+    if (["™", "©", "®"].indexOf(surrogate) > -1) {
+        return "";
+    }
+
+    try {
+        return `https://twemoji.maxcdn.com/2/svg/${Twemoji.convert.toCodePoint(surrogate)}.svg`;
+    } catch (error) {
+        return "";
+    }
+}
+
+type EmojiMap = { [key: string]: string };
+const DIVERSITY_SURROGATES = ["🏻", "🏼", "🏽", "🏾", "🏿"];
+const NAME_TO_EMOJI: EmojiMap = {};
+const EMOJI_TO_NAME: EmojiMap = {};
+
+Object.keys(Emoji).forEach((category) => {
+    Emoji[category].forEach((emoji) => {
+        EMOJI_TO_NAME[emoji.surrogates] = emoji.names[0] || "";
+
+        emoji.names.forEach((name) => {
+            NAME_TO_EMOJI[name] = emoji.surrogates;
+
+            DIVERSITY_SURROGATES.forEach((d, i) => {
+                NAME_TO_EMOJI[`${name}::skin-tone-${i + 1}`] = emoji.surrogates.concat(d);
+            });
+        });
+
+        DIVERSITY_SURROGATES.forEach((d, i) => {
+            const surrogates = emoji.surrogates.concat(d);
+            const name = emoji.names[0] || "";
+
+            EMOJI_TO_NAME[surrogates] = `${name}::skin-tone-${i + 1}`;
+        });
+    });
+});
+
+// eslint-disable-next-line no-useless-escape
+const EMOJI_NAME_AND_DIVERSITY_RE = /^:([^\s:]+?(?:::skin\-tone\-\d)?):/;
+
+function convertNameToSurrogate(name: string, t = "") {
+    // eslint-disable-next-line no-prototype-builtins
+    return NAME_TO_EMOJI.hasOwnProperty(name) ? NAME_TO_EMOJI[name] : t;
+}
+
+function convertSurrogateToName(surrogate: string, colons = true, n = "") {
+    // what is n for?
+    let a = n;
+
+    // eslint-disable-next-line no-prototype-builtins
+    if (EMOJI_TO_NAME.hasOwnProperty(surrogate)) {
+        a = EMOJI_TO_NAME[surrogate];
+    }
+
+    return colons ? `:${a}:` : a;
+}
+
+function escape(str: string) {
+    // eslint-disable-next-line no-useless-escape
+    return str.replace(/[\-\[\]\/\{}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+const replacer = (function () {
+    const surrogates = Object.keys(EMOJI_TO_NAME)
+        .sort((surrogate) => -surrogate.length)
+        .map((surrogate) => escape(surrogate))
+        .join("|");
+
+    return new RegExp("(" + surrogates + ")", "g");
+})();
+
+function translateSurrogatesToInlineEmoji(surrogates: string) {
+    return surrogates.replace(replacer, (_o, match) => convertSurrogateToName(match));
+}
+
+interface ExtendedASTNode {
+    src?: string;
+    surrogate: string;
+    jumboable?: boolean;
+    name: string;
+}
+
+type ReactASTNodeExtended = SimpleMarkdown.SingleASTNode & ExtendedASTNode;
+
+const baseRules: SimpleMarkdown.ParserRules = {
+    newline: SimpleMarkdown.defaultRules.newline,
+    paragraph: SimpleMarkdown.defaultRules.paragraph,
+    escape: SimpleMarkdown.defaultRules.escape,
+    link: SimpleMarkdown.defaultRules.link,
+    autolink: {
+        ...SimpleMarkdown.defaultRules.autolink,
+        match: SimpleMarkdown.inlineRegex(/^<(https?:\/\/[^ >]+)>/),
+    },
+    url: SimpleMarkdown.defaultRules.url,
+    strong: SimpleMarkdown.defaultRules.strong,
+    em: SimpleMarkdown.defaultRules.em,
+    u: SimpleMarkdown.defaultRules.u,
+    br: SimpleMarkdown.defaultRules.br,
+    inlineCode: SimpleMarkdown.defaultRules.inlineCode,
+    emoticon: {
+        order: SimpleMarkdown.defaultRules.text.order,
+        match: function (source) {
+            return /^(¯\\_\(ツ\)_\/¯)/.exec(source);
+        },
+        parse: function (capture) {
+            return { type: "text", content: capture[1] };
+        },
+    },
+    codeBlock: {
+        order: SimpleMarkdown.defaultRules.codeBlock.order,
+        match(source) {
+            // eslint-disable-next-line no-useless-escape
+            return /^```(([A-z0-9\-]+?)\n+)?\n*([^]+?)\n*```/.exec(source);
+        },
+        parse(capture) {
+            return { lang: (capture[2] || "").trim(), content: capture[3] || "" };
+        },
+    },
+    emoji: {
+        order: SimpleMarkdown.defaultRules.text.order,
+        match(source) {
+            return EMOJI_NAME_AND_DIVERSITY_RE.exec(source);
+        },
+        parse(capture) {
+            const match = capture[0];
+            const name = capture[1];
+            const surrogate = convertNameToSurrogate(name);
+            return surrogate
+                ? {
+                      name: `:${name}:`,
+                      surrogate: surrogate,
+                      src: getEmojiURL(surrogate),
+                  }
+                : {
+                      type: "text",
+                      content: match,
+                  };
+        },
+        // @ts-ignore
+        react(node: ReactASTNodeExtended, recurseOutput, state) {
+            return node.src ? (
+                <img
+                    draggable={false}
+                    className={`emoji ${node.jumboable ? "jumboable" : ""}`}
+                    alt={node.surrogate}
+                    title={node.name}
+                    src={node.src}
+                    key={state.key}
+                />
+            ) : (
+                <span key={state.key}>{node.surrogate}</span>
+            );
+        },
+    },
+    customEmoji: {
+        order: SimpleMarkdown.defaultRules.text.order,
+        match(source) {
+            return /^<:(\w+):(\d+)>/.exec(source);
+        },
+        parse(capture) {
+            const name = capture[1];
+            const id = capture[2];
+            return {
+                emojiId: id,
+                // NOTE: we never actually try to fetch the emote
+                // so checking if colons are required (for 'name') is not
+                // something we can do to begin with
+                name: name,
+                src: getEmoteURL({
+                    id: id,
+                }),
+            };
+        },
+        // @ts-ignore
+        react(node: ReactASTNodeExtended, recurseOutput, state) {
+            return (
+                <img
+                    draggable={false}
+                    className={`emoji ${node.jumboable ? "jumboable" : ""}`}
+                    alt={`<:${node.name}:${node.id}>`}
+                    title={node.name}
+                    src={node.src}
+                    key={state.key}
+                />
+            );
+        },
+    },
+    text: {
+        ...SimpleMarkdown.defaultRules.text,
+        parse(capture, recurseParse, state) {
+            return state.nested
+                ? {
+                      content: capture[0],
+                  }
+                : recurseParse(translateSurrogatesToInlineEmoji(capture[0]), {
+                      ...state,
+                      nested: true,
+                  });
+        },
+    },
+    s: {
+        order: SimpleMarkdown.defaultRules.u.order,
+        match: SimpleMarkdown.inlineRegex(/^~~([\s\S]+?)~~(?!_)/),
+        parse: SimpleMarkdown.defaultRules.u.parse,
+    },
+};
+
+function createRules(r: typeof baseRules): SimpleMarkdown.ParserRules {
+    const paragraph = r.paragraph;
+    const url = r.url;
+    const link = r.link;
+    const codeBlock = r.codeBlock;
+    const inlineCode = r.inlineCode;
+
+    return {
+        ...r,
+        // @ts-ignore
+        s: {
+            order: r.u.order,
+            match: SimpleMarkdown.inlineRegex(/^~~([\s\S]+?)~~(?!_)/),
+            parse: r.u.parse,
+            react(node, recurseOutput, state) {
+                // @ts-ignore
+                return <s key={state.key}>{recurseOutput(node.content, state)}</s>;
+            },
+        },
+        paragraph: {
+            ...paragraph,
+            react(node, recurseOutput, state) {
+                // @ts-ignore
+                return <p key={state.key}>{recurseOutput(node.content, state)}</p>;
+            },
+        },
+        // @ts-ignore
+        url: {
+            ...url,
+            match: SimpleMarkdown.inlineRegex(/^((https?|steam):\/\/[^\s<]+[^<.,:;"')\]\s])/),
+        },
+        link: {
+            ...link,
+            // @ts-ignore
+            react(node: SimpleMarkdown.SingleASTNode, recurseOutput, state) {
+                // this contains some special casing for invites (?)
+                // or something like that.
+                // we don't really bother here
+                const children = recurseOutput(node.content, state);
+                const title = node.title || astToString(node.content);
+                return (
+                    <a
+                        title={title}
+                        href={SimpleMarkdown.sanitizeUrl(node.target)}
+                        target="_blank"
+                        rel="noreferrer"
+                        key={state.key}
+                    >
+                        {children}
+                    </a>
+                );
+            },
+        },
+        inlineCode: {
+            ...inlineCode,
+            // @ts-ignore
+            react(node: SimpleMarkdown.SingleASTNode, recurseOutput, state) {
+                return (
+                    <code className="inline" key={state.key}>
+                        {node.content}
+                    </code>
+                );
+            },
+        },
+        codeBlock: {
+            ...codeBlock,
+            // @ts-ignore
+            react(node: SimpleMarkdown.SingleASTNode, recurseOutput, state) {
+                if (node.lang && hljs.getLanguage(node.lang) != null) {
+                    const highlightedBlock = hljs.highlight(node.lang, node.content, true);
+
+                    return (
+                        <pre key={state.key}>
+                            <code
+                                className={`hljs ${highlightedBlock.language}`}
+                                dangerouslySetInnerHTML={{ __html: highlightedBlock.value }}
+                            />
+                        </pre>
+                    );
+                }
+
+                return (
+                    <pre key={state.key}>
+                        <code className="hljs">{node.content}</code>
+                    </pre>
+                );
+            },
+        },
+    };
+}
+
+const rulesWithoutMaskedLinks = createRules({
+    ...baseRules,
+    link: {
+        ...baseRules.link,
+        match() {
+            return null;
+        },
+    },
+});
+
+// used in:
+//  message contrent
+const parse = parserFor(rulesWithoutMaskedLinks);
+
+// used in:
+//  message content (webhook mode)
+//  embed description
+//  embed field values
+const parseAllowLinks = parserFor(createRules(baseRules));
+
+// used in:
+//  embed title (obviously)
+//  embed field names
+const parseEmbedTitle = parserFor(
+    omit(rulesWithoutMaskedLinks, ["codeBlock", "br", "mention", "channel", "roleMention"])
+);
+
+// used in:
+//  message content
+function jumboify(ast: SimpleMarkdown.SingleASTNode[]) {
+    const nonEmojiNodes = ast.some(
+        (node) =>
+            node.type !== "emoji" &&
+            node.type !== "customEmoji" &&
+            (typeof node.content !== "string" || node.content.trim() !== "")
+    );
+
+    if (nonEmojiNodes) {
+        return ast;
+    }
+
+    const maximum = 27;
+    let count = 0;
+
+    ast.forEach((node) => {
+        if (node.type === "emoji" || node.type === "customEmoji") {
+            count += 1;
+        }
+
+        if (count > maximum) {
+            return false;
+        }
+    });
+
+    if (count < maximum) {
+        ast.forEach((node) => (node.jumboable = true));
+    }
+
+    return ast;
+}
+
+export { parse, parseAllowLinks, parseEmbedTitle, jumboify };
