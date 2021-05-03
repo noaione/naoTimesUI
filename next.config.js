e29653290d7bb903aa2ee8ff7189cf4be7b19144
noaione <noaione0809@gmail.com>
2021-05-03 11:10:21 +0700

    Add sentry.io support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = {
+const moduleExports = {
     future: {
         webpack5: true,
     },
     productionBrowserSourceMaps: true,
 };
+
+const SentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
