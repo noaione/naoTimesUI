d857df5eadb4846be77bdcba291995e461928e4a
noaione <noaione0809@gmail.com>
2021-05-04 11:38:24 +0700

    api: create route to delete RSS

---
+++ pages/api/fsrss/nuke.ts
@@ -0,0 +1,30 @@
+import { NextApiResponse } from "next";
+
+import withSession, { IUserAuth, NextApiRequestWithSession } from "../../../lib/session";
+import { emitSocketAndWait } from "../../../lib/socket";
+
+export default withSession(async (req: NextApiRequestWithSession, res: NextApiResponse) => {
+    const user = req.session.get<IUserAuth>("user");
+    if (!user) {
+        return res.status(403).json({ message: "Unathorized", success: false });
+    }
+    const reqBody = await req.body;
+    if (typeof reqBody.hashId !== "string") {
+        return res.status(400).json({ message: "Missing RSS hash ID", success: false });
+    }
+
+    try {
+        try {
+            const result = await emitSocketAndWait("fsrss delete", {
+                id: user.id,
+                hash: reqBody.hashId,
+            });
+            res.json({ id: result.id, success: true });
+        } catch (e) {
+            res.status(500).json({ message: e.toString(), success: false });
+        }
+    } catch (e) {
+        console.error(e);
+        res.send("an error occured");
+    }
+});
