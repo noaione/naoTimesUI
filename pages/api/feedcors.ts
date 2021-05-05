e36a2fa623c0c1627460007fbc7d071a0b033eba
noaione <noaione0809@gmail.com>
2021-05-05 07:52:37 +0700

    api(feedcors): properly set the header version

--- pages/api/feedcors.ts
+++ pages/api/feedcors.ts
@@ -4,13 +4,17 @@ import { NextApiResponse } from "next";
 import withSession, { NextApiRequestWithSession } from "../../lib/session";
 import { emitSocketAndWait } from "../../lib/socket";
 
+import packageJSON from "../../package.json";
+
+const APP_VERSION = packageJSON.version;
+
 export default withSession(async (req: NextApiRequestWithSession, res: NextApiResponse) => {
     const urlParams = new URL(req.query.url as string);
 
     try {
         const axiosResp = await axios.get(urlParams.toString(), {
             headers: {
-                "User-Agent": "naoTimesUI/1.1.0 (+https://github.com/noaione/naoTimesUI)",
+                "User-Agent": `naoTimesUI/v${APP_VERSION} (+https://github.com/noaione/naoTimesUI)`,
             },
             responseType: "text",
         });
