1853abd3b78bc811188ea9ef6b1109a135a92835
noaione <noaione0809@gmail.com>
2021-05-06 11:33:27 +0700

    postcss: readjust to follow the original config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,25 @@
 const isProd = process.env.NODE_ENV === "production";
 
-const usePlugins = ["postcss-import", "postcss-nested", "tailwindcss", "autoprefixer"];
+const usePlugins = [
+    "postcss-import",
+    "tailwindcss",
+    "postcss-flexbugs-fixes",
+    [
+        "postcss-preset-env",
+        {
+            autoprefixer: {
+                flexbox: "no-2009",
+            },
+            stage: 3,
+            features: {
+                "custom-properties": false,
+                "nesting-rules": true,
+            },
+        },
+    ],
+    "autoprefixer",
+];
+
 if (isProd) {
     usePlugins.push("cssnano");
 }
