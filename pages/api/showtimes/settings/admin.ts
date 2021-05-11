67ce91d68ade4a952a963a8bc06ec52ed7abe666
noaione <noaione0809@gmail.com>
2021-05-11 20:19:28 +0700

    api: create API route to change admin

---
+++ pages/api/showtimes/settings/admin.ts
@@ -0,0 +1,88 @@
+import { NextApiResponse } from "next";
+
+import dbConnect from "../../../../lib/dbConnect";
+import { emitSocket } from "../../../../lib/socket";
+import { isNone } from "../../../../lib/utils";
+import withSession, { IUserAuth, NextApiRequestWithSession } from "../../../../lib/session";
+
+import { ShowAdminModel, ShowtimesModel } from "../../../../models/show";
+
+async function tryToAdjustAdminData(serverId: string, newAdminIds: string[]): Promise<[string, boolean]> {
+    if (!Array.isArray(newAdminIds)) {
+        return ["Mohon berikan array of string untuk adminIds", false];
+    }
+    newAdminIds = newAdminIds.filter((e) => typeof e === "string" && e.length > 0);
+    if (newAdminIds.length < 1) {
+        return ["Admin IDs kosong, mohon isi dengan Discord ID", false];
+    }
+    let successCheck1 = true;
+    let failed = "";
+    newAdminIds.forEach((admin) => {
+        if (Number.isNaN(parseInt(admin))) {
+            successCheck1 = false;
+            failed = admin;
+        }
+    });
+    if (!successCheck1) {
+        return [`Salah satu admin ID bukanlah angka: ${failed}`, false];
+    }
+    let successCheck2 = false;
+    try {
+        const showAdmin = await ShowAdminModel.find({});
+        const serversAdminsSets: string[] = [];
+        showAdmin.forEach((res) => {
+            if (res.servers.includes(serverId)) {
+                serversAdminsSets.push(res.id);
+            }
+        });
+        serversAdminsSets.forEach((admin) => {
+            if (newAdminIds.includes(admin)) {
+                successCheck2 = true;
+            }
+        });
+    } catch (e) {
+        console.error(e);
+        return ["Gagal mengambil database, mohon coba lagi nanti", false];
+    }
+    if (!successCheck2) {
+        return ["Tidak dapat menemukan Server Admin utama dalam list baru, mohon cek lagi", false];
+    }
+    try {
+        // @ts-ignore
+        await ShowtimesModel.findOneAndUpdate(
+            { id: { $eq: serverId } },
+            { $set: { serverowner: newAdminIds } }
+        );
+    } catch (e) {
+        return ["Gagal memperbarui database, mohon coba lagi nanti", false];
+    }
+    emitSocket("pull data", serverId);
+    return ["sukses", true];
+}
+
+export default withSession(async (req: NextApiRequestWithSession, res: NextApiResponse) => {
+    const reqData = await req.body;
+    const user = req.session.get<IUserAuth>("user");
+    if (!user) {
+        res.status(403).json({ message: "Unauthorized", code: 403 });
+    } else if (!reqData) {
+        res.status(400).json({ message: "Tidak ada body yang diberikan :(", code: 400 });
+    } else if (isNone(reqData.adminIds)) {
+        res.status(400).json({ message: "Mohon berikan adminIds untuk perubahan baru", code: 400 });
+    } else {
+        await dbConnect();
+        if (user.privilege === "owner") {
+            res.status(501).json({
+                message: "Sorry, this API routes is not implemented",
+                code: 501,
+            });
+        } else {
+            const [msg, status] = await tryToAdjustAdminData(user.id, reqData.adminIds);
+            if (!status) {
+                res.status(500).json({ message: msg, code: 500 });
+            } else {
+                res.json({ message: msg, code: 200 });
+            }
+        }
+    }
+});
