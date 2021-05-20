92a6f4c48d5933d59c6dd1c99aa6836a6598885d
noaione <noaione0809@gmail.com>
2021-05-20 14:49:52 +0700

    FansubRSSOverview: animate the entry

--- components/FansubRSS/Overview.tsx
+++ components/FansubRSS/Overview.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import { FansubRSSFeeds } from "../../lib/fsrss";
 
 interface FSRSSProps {
     feed: FansubRSSFeeds;
+    animateDelay?: number;
 }
 
 class FansubRSSOverview extends React.Component<FSRSSProps> {
@@ -12,24 +14,27 @@ class FansubRSSOverview extends React.Component<FSRSSProps> {
     }
 
     render() {
-        const { feed } = this.props;
+        const { feed, animateDelay } = this.props;
         const { id, channel, feedUrl } = feed;
 
         return (
-            <>
-                <div className="p-5 bg-white dark:bg-gray-700 rounded shadow-md">
-                    <div className="flex flex-col justify-center">
-                        <a
-                            href={"/admin/fansubrss/" + id}
-                            className="text-xl font-bold text-gray-900 dark:text-gray-200 hover:underline break-all"
-                        >
-                            {feedUrl}
-                        </a>
-                    </div>
-                    <div className="text-base text-gray-400 break-all">ID: {id}</div>
-                    <div className="text-base text-gray-400 break-all">Channel ID: {channel}</div>
+            <motion.div
+                className="p-5 bg-white dark:bg-gray-700 rounded shadow-md"
+                initial={{ y: 50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ delay: animateDelay || 0.25 }}
+            >
+                <div className="flex flex-col justify-center">
+                    <a
+                        href={"/admin/fansubrss/" + id}
+                        className="text-xl font-bold text-gray-900 dark:text-gray-200 hover:underline break-all"
+                    >
+                        {feedUrl}
+                    </a>
                 </div>
-            </>
+                <div className="text-base text-gray-400 break-all">ID: {id}</div>
+                <div className="text-base text-gray-400 break-all">Channel ID: {channel}</div>
+            </motion.div>
         );
     }
 }
