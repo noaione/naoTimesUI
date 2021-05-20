55568ab37fffb5fbab2fdf87a2a53f13e981d666
noaione <noaione0809@gmail.com>
2021-05-20 15:09:34 +0700

    SampleViewer: create a way to animate the entry Need to provide animate as true to add animation fly-in entry

--- components/FansubRSS/SampleViewer.tsx
+++ components/FansubRSS/SampleViewer.tsx
@@ -1,18 +1,46 @@
+import { motion } from "framer-motion";
 import React from "react";
 
-class SampleViewerData extends React.Component<{ data: any; name: any }> {
+interface AniContainer {
+    animate?: boolean;
+    animateDelay?: number;
+}
+interface SampleViewerDataProps extends AniContainer {
+    data: any;
+    name: any;
+}
+
+function ViewerContainer(props: AniContainer & { children?: React.ReactNode }) {
+    if (props.animate) {
+        return (
+            <motion.div
+                className="flex flex-col"
+                initial={{ y: 35, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ delay: props.animateDelay || 0.2 }}
+            >
+                {props.children}
+            </motion.div>
+        );
+    }
+
+    return <div className="flex flex-col">{props.children}</div>;
+}
+
+class SampleViewerData extends React.Component<SampleViewerDataProps> {
     constructor(props) {
         super(props);
     }
 
     render() {
-        const { data, name } = this.props;
+        const { data, name, animate, animateDelay } = this.props;
         let nicerData = data;
         if (Array.isArray(data)) {
             nicerData = data.join(", ");
         }
+
         return (
-            <div className="flex flex-col">
+            <ViewerContainer animate={animate} animateDelay={animateDelay}>
                 <div className="px-2 py-1 bg-gray-200 dark:bg-gray-800 dark:text-gray-200 rounded-t-lg">
                     <strong>Key: </strong>
                     <code>{`{${name}}`}</code>
@@ -20,12 +48,12 @@ class SampleViewerData extends React.Component<{ data: any; name: any }> {
                 <div className="px-3 py-2 bg-gray-200 dark:bg-gray-800 dark:text-gray-200 rounded-b-lg">
                     {nicerData}
                 </div>
-            </div>
+            </ViewerContainer>
         );
     }
 }
 
-interface SampleViewerProps {
+interface SampleViewerProps extends AniContainer {
     sample?: { [key: string]: any };
 }
 
@@ -35,14 +63,26 @@ class SampleViewer extends React.Component<SampleViewerProps> {
     }
 
     render() {
-        const { sample } = this.props;
+        const { sample, animate } = this.props;
 
         return (
             <>
                 <label className="text-lg font-semibold dark:text-white mt-4">Sample</label>
                 <div className="flex flex-col w-full mt-2 gap-2">
-                    {Object.keys(sample).map((key) => {
-                        return <SampleViewerData name={key} data={sample[key]} key={`view-${key}`} />;
+                    {Object.keys(sample).map((key, idx) => {
+                        let aniDelay = 0.2;
+                        if (idx > 0) {
+                            aniDelay = 0.2 + 0.1 * (idx + 1);
+                        }
+                        return (
+                            <SampleViewerData
+                                name={key}
+                                data={sample[key]}
+                                key={`view-${key}`}
+                                animate={animate}
+                                animateDelay={aniDelay}
+                            />
+                        );
                     })}
                 </div>
             </>
