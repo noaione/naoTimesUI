c1278a0136505558371a175de136185aa11f74dc
noaione <noaione0809@gmail.com>
2021-05-21 22:24:35 +0700

    components: Create a component that animate only when it enter Viewport Would save some precious CPU/GPU resources

---
+++ components/MotionInView.tsx
@@ -0,0 +1,160 @@
+import React, { useEffect } from "react";
+
+import {
+    HTMLMotionProps,
+    motion,
+    MotionProps,
+    TargetAndTransition,
+    useAnimation,
+    Variants,
+} from "framer-motion";
+import { ControlsAnimationDefinition } from "framer-motion/types/animation/types";
+
+import { useInView } from "react-intersection-observer";
+import { TargetResolver } from "framer-motion/types/types";
+
+export interface MotionViewVariants extends Variants {
+    hidden?: TargetAndTransition | TargetResolver;
+    visible?: TargetAndTransition | TargetResolver;
+}
+
+function MotionInViewDiv(props: MotionProps & HTMLMotionProps<"div">) {
+    const controls = useAnimation();
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            const { animate } = props;
+            if (typeof animate !== "boolean") {
+                controls.start(animate as ControlsAnimationDefinition);
+            }
+        }
+    }, [controls, inView]);
+
+    return <motion.div {...props} ref={ref} animate={controls} />;
+}
+
+function MotionInViewMain(props: MotionProps & HTMLMotionProps<"main">) {
+    const controls = useAnimation();
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            const { animate } = props;
+            if (typeof animate !== "boolean") {
+                controls.start(animate as ControlsAnimationDefinition);
+            }
+        }
+    }, [controls, inView]);
+
+    return <motion.main {...props} ref={ref} initial="hidden" animate={controls} />;
+}
+
+function MotionInViewHead1(props: MotionProps & HTMLMotionProps<"h1">) {
+    const controls = useAnimation();
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            const { animate } = props;
+            if (typeof animate !== "boolean") {
+                controls.start(animate as ControlsAnimationDefinition);
+            }
+        }
+    }, [controls, inView]);
+
+    return <motion.h1 {...props} ref={ref} initial="hidden" animate={controls} />;
+}
+
+function MotionInViewHead2(props: MotionProps & HTMLMotionProps<"h2">) {
+    const controls = useAnimation();
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            const { animate } = props;
+            if (typeof animate !== "boolean") {
+                controls.start(animate as ControlsAnimationDefinition);
+            }
+        }
+    }, [controls, inView]);
+
+    return <motion.h2 {...props} ref={ref} initial="hidden" animate={controls} />;
+}
+
+function MotionInViewHeader(props: MotionProps & HTMLMotionProps<"header">) {
+    const controls = useAnimation();
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            const { animate } = props;
+            if (typeof animate !== "boolean") {
+                controls.start(animate as ControlsAnimationDefinition);
+            }
+        }
+    }, [controls, inView]);
+
+    return <motion.header {...props} ref={ref} initial="hidden" animate={controls} />;
+}
+
+function MotionInViewImage(props: MotionProps & HTMLMotionProps<"img">) {
+    const controls = useAnimation();
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            const { animate } = props;
+            if (typeof animate !== "boolean") {
+                controls.start(animate as ControlsAnimationDefinition);
+            }
+        }
+    }, [controls, inView]);
+
+    return <motion.img {...props} ref={ref} initial="hidden" animate={controls} />;
+}
+
+function MotionInViewButton(props: MotionProps & HTMLMotionProps<"button">) {
+    const controls = useAnimation();
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            const { animate } = props;
+            if (typeof animate !== "boolean") {
+                controls.start(animate as ControlsAnimationDefinition);
+            }
+        }
+    }, [controls, inView]);
+
+    return <motion.button {...props} ref={ref} initial="hidden" animate={controls} />;
+}
+
+function MotionInViewParagraph(props: MotionProps & HTMLMotionProps<"p">) {
+    const controls = useAnimation();
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            const { animate } = props;
+            if (typeof animate !== "boolean") {
+                controls.start(animate as ControlsAnimationDefinition);
+            }
+        }
+    }, [controls, inView]);
+
+    return <motion.p {...props} ref={ref} initial="hidden" animate={controls} />;
+}
+
+const MotionInView = {
+    button: MotionInViewButton,
+    div: MotionInViewDiv,
+    h1: MotionInViewHead1,
+    h2: MotionInViewHead2,
+    header: MotionInViewHeader,
+    img: MotionInViewImage,
+    main: MotionInViewMain,
+    p: MotionInViewParagraph,
+};
+
+export default MotionInView;
