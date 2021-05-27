02cfcefd82f4732e41e42f6bfd94f0a5c2ec87ac
noaione <noaione0809@gmail.com>
2021-05-27 13:37:14 +0700

    Try out esbuild-loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withSentryConfig } = require("@sentry/nextjs");
+const { ESBuildMinifyPlugin } = require("esbuild-loader");
 
 const GITHUB_CI = process.env.GITHUB_WORKFLOW;
 const SKIP_SENTRY = process.env.SKIP_SENTRY;
@@ -15,11 +16,57 @@ if (IS_PREVIEW) {
     skipSentry = true;
 }
 
+function useESBuildMinify(config, options) {
+    const terserIndex = config.optimization.minimizer.findIndex(
+        (minimizer) => minimizer.constructor.name === "TerserPlugin"
+    );
+    if (terserIndex > -1) {
+        config.optimization.minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
+    }
+}
+
+function useESBuildLoader(config, options) {
+    const tsLoader = config.module.rules.find((rule) => rule.test && rule.test.test(".ts"));
+    if (tsLoader && tsLoader.use && tsLoader.use.loader) {
+        tsLoader.use.loader = "esbuild-loader";
+        tsLoader.use.options = options;
+    }
+    const jsLoader = config.module.rules.find((rule) => rule.test && rule.test.test(".js"));
+    if (jsLoader && jsLoader.use && jsLoader.use.loader) {
+        jsLoader.use.loader = "esbuild-loader";
+        if (Object.keys(options).length > 0) {
+            // eslint-disable-next-line dot-notation
+            options["loader"] = "jsx";
+        }
+        jsLoader.use.options = options;
+    }
+}
+
 const moduleExports = {
     future: {
         webpack5: true,
     },
     productionBrowserSourceMaps: true,
+    webpack: (config, { webpack, isServer }) => {
+        config.plugins.push(
+            new webpack.ProvidePlugin({
+                React: "react",
+            })
+        );
+
+        useESBuildMinify(config);
+        useESBuildLoader(config, {
+            loader: "tsx",
+            target: "es2015",
+        });
+
+        if (!isServer) {
+            config.resolve.fallback.fs = false;
+            config.resolve.fallback.net = false;
+        }
+
+        return config;
+    },
     async headers() {
         return [
             {
