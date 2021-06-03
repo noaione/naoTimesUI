dad339010ab6d50aa28962f80a22d23291dbcdfe
noaione <noaione0809@gmail.com>
2021-06-03 22:25:38 +0700

    pages(document): rename the class

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -51,7 +51,7 @@ const toggleTheme = function() {
 };
 `;
 
-class MyDocument extends Document {
+class NaoTimesAppDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
         const initialProps = await Document.getInitialProps(ctx);
         return { ...initialProps };
@@ -86,4 +86,4 @@ class MyDocument extends Document {
     }
 }
 
-export default MyDocument;
+export default NaoTimesAppDocument;
