8c64425f6bd4bf6cda791fbb5e8477c50ac8843b
noaione <noaione0809@gmail.com>
2021-07-01 15:03:06 +0700

    Sentry: try to import just the init

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,11 +2,11 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import { init } from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
+init({
     dsn: SENTRY_DSN || "https://280820ff6eeb4b358d20cb8fc5e83b42@o568506.ingest.sentry.io/5713654",
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
