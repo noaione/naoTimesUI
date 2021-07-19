eb7c146a995070ae14a15596f0413c0618e2fab8
noaione <noaione0809@gmail.com>
2021-07-19 20:28:42 +0700

    types(collab): fix typing

--- types/collab.ts
+++ types/collab.ts
@@ -1,9 +1,22 @@
 import { ShowAnimeProps } from "@/models/show";
 
+interface SimpleServerInfo {
+    id: string;
+    name: string;
+}
+
 export interface CollabData {
+    id: string;
+    title: string;
+    image: string;
+    collaborations: SimpleServerInfo[];
+}
+
+export interface KonfirmasiData {
     id: string;
     serverId: string;
     animeInfo: ShowAnimeProps;
 }
 
+export type Confirmations = KonfirmasiData[];
 export type Collaborations = CollabData[];
