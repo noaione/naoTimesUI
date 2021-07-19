f624d8f578bdcd63f874b28f4f3c7573a20bfa3b
noaione <noaione0809@gmail.com>
2021-07-19 12:57:50 +0700

    Bump version of multiple packages - Update NextJS to v11 - Update Tailwindcss to v2.2 - Bump multiple deps version

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
         "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended",
         "prettier",
+        "next",
     ],
     env: {
         node: true,
