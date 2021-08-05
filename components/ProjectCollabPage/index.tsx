3817c48687ffb8b30fc8568389490a9b303ae6ba
noaione <noaione0809@gmail.com>
2021-08-05 19:40:57 +0700

    component: add Sever target button

--- components/ProjectCollabPage/index.tsx
+++ components/ProjectCollabPage/index.tsx
@@ -1,9 +1,11 @@
 import CollabCardComponent from "./CollabCard";
+import CollabSeverTargetButton from "./CollabSeverTarget";
 import CollabTargetConfirmComponent from "./CollabTargetConfirm";
 
 const ProjectCollabComponent = {
     CollabCard: CollabCardComponent,
     CollabTargetConfirm: CollabTargetConfirmComponent,
+    CollabSever: CollabSeverTargetButton,
 };
 
 export default ProjectCollabComponent;
