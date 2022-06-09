ea900205ac35f02f61ef691c90e37caf5dfa479c
noaione <noaione0809@gmail.com>
2022-06-09 18:50:37 +0800

    auith(reset): consistency check

--- pages/api/auth/reset.ts
+++ pages/api/auth/reset.ts
@@ -7,15 +7,15 @@ export default withSession(async (req: NextApiRequestWithSession, res: NextApiRe
     const reqData = await req.body;
     const user = req.session.get<IUserAuth>("user");
     if (!user) {
-        res.status(403).json({ message: "Unauthorized", code: 403 });
+        res.status(403).json({ message: "Unauthorized", code: 403, success: false });
     } else if (!reqData) {
-        res.status(400).json({ message: "Tidak ada body yang diberikan :(", code: 400 });
+        res.status(400).json({ message: "Tidak ada body yang diberikan :(", code: 400, success: false });
     } else {
         const oldUserData = await prisma.showtimesuilogin.findFirst({
             where: { id: user.id },
         });
         if (oldUserData.secret !== reqData.old) {
-            res.status(400).json({ message: "Password lama salah!", code: 400 });
+            res.status(400).json({ message: "Password lama salah!", code: 4001, success: false });
         } else {
             await prisma.showtimesuilogin.update({
                 where: { mongo_id: oldUserData.mongo_id },
@@ -23,7 +23,7 @@ export default withSession(async (req: NextApiRequestWithSession, res: NextApiRe
                     secret: reqData.new,
                 },
             });
-            res.json({ message: "success", code: 200 });
+            res.json({ message: "success", code: 200, success: true });
         }
     }
 });
