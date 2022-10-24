219d9d466d39067cb294b6600be36d3f2a5246df
noaione <noaione0809@gmail.com>
2022-10-24 17:55:07 +0800

    fix(markdown): auto slug h1, h2, and h3

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -18,6 +18,10 @@ function MarkdownLinkFormatting({ href, ...rest }: React.HTMLProps<HTMLAnchorEle
     return <a href={href} {...rest} rel="noopener noreferrer" target="_blank" />;
 }
 
+function slugify(text: string) {
+    return text.toLowerCase().replace(/[^a-z0-9]/g, "-");
+}
+
 // A wrapper around ReactMarkdown :)
 export default class Markdown extends React.Component<React.PropsWithChildren<{}>> {
     constructor(props) {
@@ -30,6 +34,27 @@ export default class Markdown extends React.Component<React.PropsWithChildren<{}
                 className="react-md"
                 components={{
                     a: ({ ...props }) => <MarkdownLinkFormatting {...props} />,
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    h2: ({ node, ...props }) => {
+                        const { id, ...rest } = props;
+                        const actualId =
+                            id ?? slugify(String(rest.children[0]).toLowerCase().replace(/ /g, "-"));
+                        return <h2 id={actualId} {...rest} />;
+                    },
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    h1: ({ node, ...props }) => {
+                        const { id, ...rest } = props;
+                        const actualId =
+                            id ?? slugify(String(rest.children[0]).toLowerCase().replace(/ /g, "-"));
+                        return <h1 id={actualId} {...rest} />;
+                    },
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    h3: ({ node, ...props }) => {
+                        const { id, ...rest } = props;
+                        const actualId =
+                            id ?? slugify(String(rest.children[0]).toLowerCase().replace(/ /g, "-"));
+                        return <h3 id={actualId} {...rest} />;
+                    },
                 }}
                 remarkPlugins={[gemoji, gfm, breaks]}
             >
